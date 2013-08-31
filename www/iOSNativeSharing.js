9c7ff6fe92b46b14125b32a40af2943efd3397e1
Sidney Bofah <sidney.bofah@googlemail.com>
2013-08-31 11:22:56 +0200

    Updated files

--- www/iOSNativeSharing.js
+++ www/iOSNativeSharing.js
@@ -8,6 +8,11 @@
 function iOSNativeSharing() {
 };
 
+function validUrl(s) {
+	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	return regexp.test(s);
+}
+
 iOSNativeSharing.prototype.available = function(callback) {
 	cordova.exec(function(avail) {
 		callback(avail ? true : false);
@@ -15,7 +20,12 @@ iOSNativeSharing.prototype.available = function(callback) {
 };
 
 iOSNativeSharing.prototype.share = function(message, url, image) {
-    cordova.exec(null, null, "iOSNativeSharing", "share", [message, image, url]);
+	if validUrl(url) {
+    		cordova.exec(null, null, "iOSNativeSharing", "share", [message, image, url]);
+	} else {
+		url = "http://" + url;
+		cordova.exec(null, null, "iOSNativeSharing", "share", [message, image, url]);
+	}
 };
     
 iOSNativeSharing.install = function() {
